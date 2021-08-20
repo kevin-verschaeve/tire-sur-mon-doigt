00e5704c2bc369987bd8721fe1882135e69b5605
Kevin Verschaeve <kverschaeve@interdrinks.fr>
2021-08-20 23:46:39 +0200

    Fix firefox and display in mobile

--- app.js
+++ app.js
@@ -5,35 +5,39 @@ let offset = -200;
 let previousCursorX = 0;
 let cursorX;
 
-document.getElementById('doigt').addEventListener('dragstart', function(event) {
-    cursorX = event.clientX;
-}, false);
+['dragstart', 'touchstart'].forEach(function (eventName) {
+    document.getElementById('doigt').addEventListener(eventName, function(event) {
+        cursorX = event.clientX || event.touches[0].clientX;
+    }, false);
+});
 
-document.getElementById('doigt').addEventListener('drag', function(event) {
-    previousCursorX = cursorX;
-    cursorX = event.clientX;
+['dragover', 'touchmove'].forEach(function (eventName) {
+    document.getElementById('doigt').addEventListener(eventName, function(event) {
+        previousCursorX = cursorX;
+        cursorX = event.clientX || event.touches[0].clientX;
 
-    let newOffset;
-    if (cursorX > previousCursorX) {
-        newOffset = offset + step;
-    } else {
-        newOffset = offset - step;
-    }
+        let newOffset;
+        if (cursorX > previousCursorX) {
+            newOffset = offset + step;
+        } else {
+            newOffset = offset - step;
+        }
 
-    event.target.style.marginLeft = newOffset;
-    offset = newOffset;
+        event.target.style.marginLeft = newOffset;
+        offset = newOffset;
 
-    if (offset >= 0) {
-        fart  = new Audio(`farts/fart${Math.floor(Math.random() * 9 + 1)}.mp3`);
-        fart.play();
+        if (offset >= 0) {
+            fart  = new Audio(`farts/fart${Math.floor(Math.random() * 9 + 1)}.mp3`);
+            fart.play();
+            event.target.style.marginLeft = -200;
+            offset = -200;
+        }
+    }, false);
+});
+
+['dragend', 'touchend'].forEach(function (eventName) {
+    document.getElementById('doigt').addEventListener(eventName, function(event) {
         event.target.style.marginLeft = -200;
         offset = -200;
-        event.preventDefault();
-        return false;
-    }
-}, false);
-
-document.getElementById('doigt').addEventListener('dragend', function(event) {
-    event.target.style.marginLeft = -200;
-    offset = -200;
-}, false);
+    }, false);
+});
