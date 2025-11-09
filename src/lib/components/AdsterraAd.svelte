7f1c0adb37099b8f371baf3cece0bd86ccbdfda2
Kevin Verschaeve <kevin.verschaeve@live.fr>
2025-11-09 15:14:13 +0100

    Add a small add on the footer

---
+++ src/lib/components/AdsterraAd.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let key;
+  export let height;
+  export let width;
+  export let format = 'iframe';
+
+  // Create the HTML string with inline scripts
+  const adHTML = `
+    <script type="text/javascript">
+      atOptions = {
+        'key': '${key}',
+        'format': '${format}',
+        'height': ${height},
+        'width': ${width},
+        'params': {}
+      };
+    <\/script>
+    <script type="text/javascript" src="//www.highperformanceformat.com/${key}/invoke.js"><\/script>
+  `;
+</script>
+
+<div class="ad-container">
+  {@html adHTML}
+</div>
+
+<style>
+  .ad-container {
+    display: flex;
+    justify-content: center;
+    margin: 1rem 0;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
-    import '$lib/assets/css/app.css'
+    import '$lib/assets/css/app.css';
+    import AdsterraAd from '$lib/components/AdsterraAd.svelte';
 </script>
 
 <h1>
@@ -7,3 +8,7 @@
 </h1>
 
 <slot></slot>
+
+<footer>
+    <AdsterraAd key={'f0b141405144a009efe3f8f2bbb97e2c'} width={728} height={90} />
+</footer>
